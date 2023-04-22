eb8608ed1dab11095a686bb511d1ca734dab5209
Huzefa1122 <muhammadhuzefa2003@gmail.com>
2023-04-22 21:08:38 +0500

    Connected Funtionality to add dynamic Data

--- src/pages/api/generate.js
+++ src/pages/api/generate.js
@@ -1,29 +1,30 @@
 import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey:
+    process.env.OPENAI_API_KEY ||
+    "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",
 });
 const openai = new OpenAIApi(configuration);
+console.log(openai);
+export const handler = async (data) => {
+  const animal = data;
 
-export default async function (req, res) {
   if (!configuration.apiKey) {
-    res.status(500).json({
+    return {
       error: {
         message:
           "OpenAI API key not configured, please follow instructions in README.md",
       },
-    });
-    return;
+    };
   }
 
-  const animal = req.body.animal || "";
   if (animal.trim().length === 0) {
-    res.status(400).json({
+    return {
       error: {
-        message: "Please enter a valid animal",
+        message: "Enter valid Search",
       },
-    });
-    return;
+    };
   }
 
   try {
@@ -34,70 +35,51 @@ export default async function (req, res) {
         {
           role: "user",
           content: ` Please provide me with a list of blogs, research papers, and videos related to the topic "${topics}", formatted as a JSON object. The output should follow this exact structure:
-
-      json
-      Copy code
-      {
-          "Query Name": {
-              "blogs": [
-                  {
-                      "title": "Dummy Blog Title",
-                      "url": "Dummy Blog URL"
-                  }
-              ],
-              "research_papers": [
-                  {
-                      "title": "Dummy Research Paper Title",
-                      "authors": "Dummy Author Names",
-                      "url": "Dummy Research Paper URL"
-                  }
-              ],
-              "authors": [
-                  {
-                      "name": "Dummy Author Name",
-                      "url": "Dummy Author URL"
-                  }
-              ],
-              "videos": [
-                  {
-                      "title": "Dummy Video Title",
-                      "url": "Dummy Video URL"
-                  }
-              ]
-          }
-      }`,
+  
+    json
+    Copy code
+    {
+        "Query Name": {
+            "blogs": [
+                {
+                    "title": "Dummy Blog Title",
+                    "url": "Dummy Blog URL"
+                }
+            ],
+            "research_papers": [
+                {
+                    "title": "Dummy Research Paper Title",
+                    "authors": "Dummy Author Names",
+                    "url": "Dummy Research Paper URL"
+                }
+            ],
+            "authors": [
+                {
+                    "name": "Dummy Author Name",
+                    "url": "Dummy Author URL"
+                }
+            ],
+            "videos": [
+                {
+                    "title": "Dummy Video Title",
+                    "url": "Dummy Video URL"
+                }
+            ]
+        }
+    }`,
         },
       ],
     });
+    console.log(completion);
 
-    res.status(200).json({ result: completion.data.choices[0].text });
-
-    console.log(completion.data.choices);
-
-    /**
-     *
-     * {
-     *
-     *  "blogs": [],
-     *  "research_papers": [],
-     *  "authors": [],
-     *  "vidoes": [],
-     *
-     * }
-     *
-     */
+    return {
+      body: completion.data.choices[0].message,
+    };
   } catch (error) {
-    // Consider adjusting the error handling logic for your use case
-    if (error.response) {
-      console.error(error.response.status, error.response.data);
-      res.status(error.response.status).json(error.response.data);
+    if (error) {
+      return error;
     } else {
       console.error(`Error with OpenAI API request: ${error.message}`);
-      res.status(500).json({
-        error: {
-          message: "An error occurred during your request.",
-        },
-      });
     }
   }
-}
+};
